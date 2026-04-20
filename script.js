6ee53fb309f7bfcb0c93966ac9a630e2e5ef1e82
Ishwarya-2008 <rsavariammal683@gmail.com>
2026-04-20 15:36:32 +0530

    Changes fixed

--- script.js
+++ script.js
@@ -1,13 +1,73 @@
-$(function () {
-    $(".box").resizable({
+// $(function () {
+//     $(".box").resizable({
+//         minWidth: 100,
+//         minHeight: 100,
+//         containment:"parent",
+//         handles: "e, s, w, ne, se, sw, nw",
+//         resize: function (event, ui) {
+//             var w = Math.round(ui.size.width);
+//             var h = Math.round(ui.size.height);
+//             $(this).find(".resize-handle").text(w + " x " + h);
+//         }
+//     });
+// });
+
+$(function(){
+    $(".box-1").resizable({
+        handles: "e, w",
         minWidth: 100,
         minHeight: 100,
-        containment:"parent",
-        handles: "e, s, w, ne, se, sw, nw",
-        resize: function (event, ui) {
+        containment: "parent",
+        resize: function(event, ui){
             var w = Math.round(ui.size.width);
-            var h = Math.round(ui.size.height);
-            $(this).find(".size-info").text(w + " x " + h);
+            $(this).find(".resize-handle").text("Width: " + w + "px");
         }
     });
-});
+
+    $(".box-2").resizable({
+        handles: "s",
+        minWidth: 100,
+        minHeight: 100,
+        containment: "parent",
+        resize: function(event, ui){
+            var h = Math.round(ui.size.height);
+            $(this).find(".resize-handle").text("Height: " + h + "px");
+        }
+    });
+
+    $(".box-3").resizable({
+        handles: "all",
+        minWidth: 100,
+        minHeight: 100,
+        containment: "parent",
+        resize: function(event, ui){
+            var w = Math.round(ui.size.width);
+            var h = Math.round(ui.size.height);
+            $(this).find(".resize-handle").text(w + " x " + h);
+        }
+    });
+
+    $(".box-4").resizable({
+        handles: "sw",
+        minWidth: 100,
+        minHeight: 100,
+        containment: "parent",
+        resize: function(event, ui){
+            var w = Math.round(ui.size.width);
+            var h = Math.round(ui.size.height);
+            $(this).find(".resize-handle").text(w + " x " + h);
+        }
+    });
+
+    $(".box-5").resizable({
+        handles: "se",
+        minWidth: 100,
+        minHeight: 100,
+        containment: "parent",
+        resize: function(event, ui){
+            var w = Math.round(ui.size.width);
+            var h = Math.round(ui.size.height);
+            $(this).find(".resize-handle").text(w + " x " + h);
+        }
+    });
+})
